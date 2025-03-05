47f14434aa4148a865528ebb2068b50257456977
Praveen <156374521+praveen00219@users.noreply.github.com>
2025-03-06 00:31:56 +0530

    Update Login.js

--- frontend/src/Components/Auth/Login.js
+++ frontend/src/Components/Auth/Login.js
@@ -12,7 +12,7 @@ function Login() {
     e.preventDefault();
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://expense-tracker-backend-1rz0.onrender.com/api/auth/login",
         {
           email,
           password,
