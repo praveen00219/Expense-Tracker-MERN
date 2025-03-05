2beb3dd8a755afcf0bb058b910088ab647d7fb84
praveen00219 <paru2192000@gmail.com>
2025-03-06 00:52:57 +0530

    updated url

--- frontend/src/Components/Auth/Signup.js
+++ frontend/src/Components/Auth/Signup.js
@@ -12,11 +12,15 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5000/api/auth/signup", {
-        name,
-        email,
-        password,
-      });
+      await axios.post(
+        // "http://localhost:5000/api/auth/signup",
+        "https://expense-tracker-backend-1rz0.onrender.com/api/auth/signup",
+        {
+          name,
+          email,
+          password,
+        }
+      );
       navigate("/login");
     } catch (err) {
       console.error(err.response?.data?.error || "Signup failed");
--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -1,7 +1,8 @@
 import React, { useContext, useState } from "react";
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/v1/";
+// const BASE_URL = "http://localhost:5000/api/v1/";
+const BASE_URL = "https://expense-tracker-backend-1rz0.onrender.com/api/v1/";
 
 const GlobalContext = React.createContext();
 
