dba8ed5543e502900f5d02e1a5d14fa9b7c76512
Praveen <156374521+praveen00219@users.noreply.github.com>
2025-03-06 00:33:05 +0530

    Update Signup.js

--- frontend/src/Components/Auth/Signup.js
+++ frontend/src/Components/Auth/Signup.js
@@ -12,7 +12,7 @@ const Signup = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://localhost:5000/api/auth/signup", {
+      await axios.post("https://expense-tracker-backend-1rz0.onrender.com/api/auth/signup", {
         name,
         email,
         password,
